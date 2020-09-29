b944de68ff018aeef2d0ac50cab0625f7e2dfb13
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-09-29 12:58:36 +0100

    customer Api working!

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const app = express();
 const customerRouter = require('./controllers/customers')
 
 
-app.use('/api/customers',customerRouter)
+//mongoose config
+const mongoose = require('mongoose');
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true,useUnifiedTopology: true  }).then(success =>{
+  console.log(`connected at ${uri}`)
+}
+).catch( error =>{
+  console.log(error)
+})
+
+
+
+app.use('/customers',customerRouter)
 
 module.exports = app
--- controllers/customers.js
+++ controllers/customers.js
@@ -1,8 +1,21 @@
 const customerRouter = require(`express`).Router();
+const Customer = require('../models/Customer')
 
-customerRouter.get('/', (_request, response) => {
-    response.status(200).json({"ello":"mate"})
+//gets all customers and returns a list
+customerRouter.get('/', async (_request, response) => {
+    const cutomers =  await Customer.find({})
+    response.status(200).json(cutomers)
  })
 
+ //finds customers by MongoId provided ID
+ //Chose to do this because easier, more stable to add, delete update
+ //also eaiest way to not get duplicate IDs
+customerRouter.get('/:id',async (request, response) => {
+    console.log(request.params.id)
+    const id = request.params.id
+    const customer = await Customer.findById(id)
+    const {first_name,last_name,email} = customer._doc
+    response.status(200).json({first_name,last_name,email})
+})
 
 module.exports = customerRouter
