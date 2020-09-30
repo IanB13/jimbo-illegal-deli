911f1a34b2ff18dfaf64f27790c009cd3f76b6b7
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-09-30 21:02:27 +0100

    inventory testing

--- testing/inventory.test.js
+++ testing/inventory.test.js
@@ -14,8 +14,43 @@ test("database reset ", async () => {
     expect(inventory.body.length).toBe(20)
 })
 
-test("")
+test("Adding item ", async () => {
+    await api
+        .post("/inventory")
+        .send(inventoryItem)
+        .expect(200)
+
+    const inventory = await api.get("/inventory")
+    expect(inventory.body[20].item).toBe("hand_sanitizer")
+    expect(inventory.body[20].details.currency_code).toBe("IDR")
+})
+
+test("converting currency", async ()  => {
+    await api
+        .put("/inventory/currency")
+        .send({ code:"USD" })
+        .expect(200)
+
+    const inventory = await api.get("/inventory")
+    expect(inventory.body[3].details.currency_code).toBe("USD")
+})
 
 afterAll(() => {
     mongoose.connection.close()
 })
+
+const inventoryItem = {
+    "details": {
+        "price": 10.00,
+        "amount": 500,
+        "color": "Green",
+        "color_hex": "#302"
+    },
+    "supplier_details": {
+        "contact": { "phone": "771-667-3249", "email": "cchiversf@kickstarter.com" },
+        "country": "Indonesia",
+        "country_code": "ID",
+        "currency": "Rupiah"
+    },
+    "item": "hand_sanitizer"
+}
