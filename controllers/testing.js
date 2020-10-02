708b9a87ec8eae2285acd03fccec3bfd9ce2bc95
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-10-02 16:57:53 +0100

    Updated api docs

--- controllers/testing.js
+++ controllers/testing.js
@@ -7,9 +7,7 @@ const invJSON = require("../inventory.json")
 const addCurrencyCodes = require("../utils/addCurrencyCodes")
 const addPasswords = require("../utils/addPasswords")
 
-//TODO:MAYBE: figure out how to run concurently, promise.all?
-//TODO: Status codes
-//TODO: make it so it only runs in testing enviroment
+
 testingRouter.get("/reset", async (_request, response) => {
     await Inventory.deleteMany({})
     //modifications to invJSON, creating more data for ease of currency conversion
