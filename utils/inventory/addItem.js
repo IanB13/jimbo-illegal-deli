f72f208c60ba5c93e93acd4b30e4ba4f2fb6093a
IanB13 <48295744+IanB13@users.noreply.github.com>
2020-10-03 15:47:40 +0100

    added auto date

--- utils/inventory/addItem.js
+++ utils/inventory/addItem.js
@@ -1,13 +1,18 @@
 const Inventory = require("../../models/Inventory")
 const addCurrencyCodes = require("./addCurrencyCodes")
 
+//adds an item to the inventory
 const createInventory = async (item) => {
     //TODO: Add data checking
-    //TODO: Add auto Date
 
     //adds currency codes
     const currencyItemArray = await addCurrencyCodes([item])
     const currencyItem = currencyItemArray[0]
+
+    //Adds date if not included
+    if(!currencyItem.details.last_purchased){
+        currencyItem.details.last_purchased = Date.now()
+    }
     await Inventory.create(currencyItem)
     return currencyItem
 }
